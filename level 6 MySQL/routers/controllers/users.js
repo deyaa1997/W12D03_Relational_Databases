1c7a3763ae69bcff152a8294858761eb03b221e4
deyaa1997 <deyah.mosa@gmail.com>
2021-06-16 10:06:17 +0300

    Finish ticket 2

--- level 6 MySQL/routers/controllers/users.js
+++ level 6 MySQL/routers/controllers/users.js
@@ -1,26 +1,26 @@
 const usersModel = require('./../../db/models/users');
+const bcrypt = require("bcrypt");
+const connection = require("../../db/db");
 
-const createNewAuthor = (req, res) => {
-	const { firstName, lastName, age, country, email, password, role } = req.body;
 
-	const user = new usersModel({
-		firstName,
-		lastName,
-		age,
-		country,
-		email,
-		password,
-		role,
-	});
+const createNewAuthor = async(req, res) => {
+	const query = `INSERT INTO users (firstName ,lastName, age , country , email , password ,role_id) VALUES (? , ? , ? , ? ,? , ? , 4)`;
+	let pass = await bcrypt.hash(req.body.password, 10); 
+	  
 
-	user
-		.save()
-		.then((result) => {
-			res.status(201).json(result);
-		})
-		.catch((err) => {
-			res.send(err);
-		});
+    console.log("pass" ,pass)
+  const data = [req.body.firstName , req.body.lastName , req.body.age , req.body.country,req.body.email , pass ] ;
+  connection.query(query, data, (err, results) => {
+	  if (err) throw err
+    console.log(results);
+  });
+  const query1 = `select * from users where email = ?`
+  const data1 =[req.body.email]
+  connection.query(query1, data1, (err, results) => {
+	if (err) throw err
+  console.log(results);
+  res.json(results)
+});
 };
 
 module.exports = {
