049db2d17e381062d506804beb10c9e15bf1af8b
deyaa1997 <deyah.mosa@gmail.com>
2021-06-16 10:35:37 +0300

    Finish ticket 3

--- level 6 MySQL/routers/controllers/auth.js
+++ level 6 MySQL/routers/controllers/auth.js
@@ -1,21 +1,46 @@
-const usersModel = require('./../../db/models/users');
+const usersModel = require("./../../db/models/users");
+const bcrypt = require("bcrypt");
+const connection = require("../../db/db");
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
 
 const login = (req, res) => {
-	const { email, password } = req.body;
+  const query = `SELECT * FROM users where email =?`;
+  const data = [req.body.email];
+  connection.query(query, data, async (err, results) => {
+    if (err) throw err;
+    if (results.length === 0) {
+      res.status(404);
+      res.json("Email is not found");
+    } else {
+      await bcrypt.compare(
+        req.body.password,
+        results[0].password,
+        (err, result) => {
+          if (result === true) {
+            const SECRET = process.env.SECRET;
+            const TOKEN_EXP_Time = process.env.TOKEN_EXP_Time;
 
-	usersModel
-		.authenticateBasic(email, password)
-		.then((result) => {
-			if (result[1] === 200)
-				return res.status(result[1]).json({ token: result[0] });
+              const payload = {
+                id: results[0].id,
+                role: results[0].role_id,
+              };
 
-			res.status(result[1]).json(result[0]);
-		})
-		.catch((err) => {
-			res.send(err);
-		});
+              const options = {
+                expiresIn: TOKEN_EXP_Time,
+              };
+              res.json(jwt.sign(payload, SECRET, options)) 
+          } else {
+            res.json("Password is incorrect");
+            res.status(403);
+          }
+        }
+      );
+    }
+  });
 };
 
 module.exports = {
-	login,
+  login,
 };
